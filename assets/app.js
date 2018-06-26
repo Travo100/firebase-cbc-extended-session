b54bd896d6a080a4113cfd851e6d8123500a34d6
Travo100 <travo100@gmail.com>
2018-06-26 16:42:35 -0700

    Added firebase and bootstrap to our project

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,24 @@
-console.log("Hello Firebase");
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBorA86SUsfJLcjXtKsjkqLMXdPaACj0Bc",
+    authDomain: "fir-cbc-extended-session.firebaseapp.com",
+    databaseURL: "https://fir-cbc-extended-session.firebaseio.com",
+    projectId: "fir-cbc-extended-session",
+    storageBucket: "",
+    messagingSenderId: "715803556482"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+database.ref().set({
+    test: "Hello!"
+});
+
+// movie reviewer
+
+// allows a user or any user
+
+// to add a movie with a title, rating, and short review of the film
+
+// that anyone can see in a table
