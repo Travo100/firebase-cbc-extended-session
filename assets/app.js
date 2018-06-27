76c632b10676b900eb202207d274c78121c52e91
Travo100 <travo100@gmail.com>
2018-06-26 17:18:12 -0700

    Added firebase code some DOM manipulation

--- assets/app.js
+++ assets/app.js
@@ -11,10 +11,6 @@ var config = {
 
 var database = firebase.database();
 
-database.ref().set({
-    test: "Hello!"
-});
-
 // movie reviewer
 
 // allows a user or any user
@@ -22,3 +18,27 @@ database.ref().set({
 // to add a movie with a title, rating, and short review of the film
 
 // that anyone can see in a table
+
+// when someone submit the form
+$("#form-submit").on("click", function(e){
+    e.preventDefault();
+    var movieTitle = $("#movie-title").val().trim();
+    var movieRating = $("#movie-rating").val().trim();
+    var movieReview = $("#movie-review").val().trim();
+
+    // get the user input from the title, rating, and short review fields
+    database.ref().push({
+        title: movieTitle,
+        rating: movieRating,
+        review: movieReview
+    });
+});
+
+
+// get all the records from the firebase database 
+// and display that info on the table
+
+database.ref().on("child_added", function(snapshot){
+    console.log(snapshot.val());
+    $("table tbody").append("<tr><td>"+snapshot.val().title+"</td><td>"+snapshot.val().rating+"/10</td><td>"+snapshot.val().review+"</td></tr>");
+});
